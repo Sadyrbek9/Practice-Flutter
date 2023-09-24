b54527cc336a74e32b0519c875d384e469599583
Sadyrbek <sadyrbek.momunaliev.bj@gmail.com>
2023-09-24 11:48:12 +0200

    Заголовок на 6 листа выполнено

--- dynamic_listview_2/lib/main.dart
+++ dynamic_listview_2/lib/main.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,19 +11,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'ListView',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
+      title: 'ListView',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          centerTitle: true,
+          title: const Text('Building ListView'),
         ),
-        home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.blueAccent,
-            centerTitle: true,
-            title: const Text('Building ListView'),
-          ),
-          body: const BodyListView(),
-        ),);
+        body: const BodyListView(),
+      ),
+    );
   }
 }
 
@@ -34,25 +36,64 @@ class BodyListView extends StatelessWidget {
     return _myListView();
   }
 }
-Widget _myListView(){
-    final List<String> items = List<String>.generate(10000, (index) => 'Item $index');
-     // принял два Параметра:длина списка и генератор, кот. принимает int (index) => возвратил $index
+//после создание абстрактного класса передали в List <String> - List <ListItem>
+Widget _myListView() {
+  final List<ListItem> items = List<ListItem>.generate(
+    10000,
+    (index) => index % 6 == 0
+        ? HeadingItem('Heading $index')
+        : MessageItem('Sender $index', 'Message body $index'),
+  );
+  // если остаток от деления 6 равно 0, выдай Heading Заголовок, иначе выдай MessageItem - обычный текст
 
+  return ListView.builder(
+    itemCount: items.length,
+    itemBuilder: (context, index) {
+      final item = items [index]; //peremennyi tyzyp aldyk
 
-    return ListView.builder(
-      itemCount: items.length,
-      itemBuilder: (context, index) {
-        return Card(
-          child: ListTile(
-            title: Text('${items[index]}'),
-            leading: const Icon(Icons.insert_photo, color: Colors.red,),
-            trailing: const Icon(Icons.keyboard_arrow_right),
-          ),
+    // emi tekwerebyz
+if (item is HeadingItem){
+  return ListTile(
+    title: Text(item.heading, style: Theme.of(context).textTheme.headlineLarge),);
+  
+}else if (item is MessageItem){
+  return ListTile(
+    title: Text(item.sender),
+    subtitle: Text(item.body),
+    leading: const Icon(Icons.insert_photo, color: Colors.red,),
+    trailing: const Icon(Icons.keyboard_arrow_right),
         );
-      },
+}
+return null;
 
-    ); //исполз. для отображения списка
-     
+      // по новому отображаем LitView, Card убрали
+      // return Card(
+      //   child: ListTile(
+      //     title: Text('${items[index]}'),
+      //     leading: const Icon(
+      //       Icons.insert_photo,
+      //       color: Colors.red,
+      //     ),
+      //     trailing: const Icon(Icons.keyboard_arrow_right),
+      //   ),
+      // );
+    },
+  ); //исполз. для отображения списка
+}
+
+abstract class ListItem{
 
 }
-Widget _
+
+class HeadingItem implements ListItem{
+    HeadingItem(this.heading,
+  );
+    final String heading;
+}
+class MessageItem implements ListItem{
+    MessageItem(
+    this.sender, this.body
+  );
+  final String sender;
+  final String body;
+}
